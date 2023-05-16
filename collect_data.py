21ddffaae40612966a402ef75d934e2a6a6a449c
sw-programmer <swgger@kaist.ac.kr>
2023-05-16 18:54:16 +0000

    Fix errors & Update readme

--- collect_data.py
+++ collect_data.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import os
 import argparse
 from tqdm import tqdm
+import wave
 
 import pandas as pd
 import numpy as np
@@ -9,6 +10,7 @@ import pafy
 from pydub import AudioSegment
 import moviepy.editor as moviepy
 import yt_dlp as youtube_dl
+# import youtube_dl
 from scipy.io.wavfile import read as read_wav
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -20,46 +22,42 @@ def get_audio(url:str) -> str:
     Returns:
         str: Path to full audio data.
     """
-
     if isinstance(url, str):
-        ydl_opts = {
+        with youtube_dl.YoutubeDL({}) as ydl:                   # get metadata to designate output path
+            meta = ydl.extract_info(url, download=False) 
+        path = f"./Data/wav/{meta['title']}.{meta['format']}"
+
+        ydl_opts = {                                            # get audio(.wav w/ 16kHz sampling rate) into designated output path
             'format': 'bestaudio/best',
+            'outtmpl': path,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '16',                                               # convert video into mp3 file w/ 16kHz bitrate.
+                'preferredcodec': 'wav'
             }],
+            'postprocessor_args': [
+                '-ar', '16000'
+            ]
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-        path = f'./Data/wav/{vid.title}'
-        assert not os.path.isfile(path), "Given url is already converted into audio."   # raise error if file already exists
+        path += '.wav'
+        assert wave.open(path, "rb").getframerate() == 16000
 
-        # rate = aud.rawbitrate
-        # assert rate >= 16000, "Sampling rate for given audio is not sufficient."        # raise error if sampling rate is below 16kHz
-
-        #TODO: 이게 되는지 확인 -> 안되면 aud를 webm로 저장 이후 파일 경로 사용 후 바로 삭제
-        aud_seg = AudioSegment.from_file(aud)
-        aud_seg.export(path, format='wav')
-
-    #TODO: url list 로 주어질 경우 고려 필요
-    else:
+    else:   #TODO
         pass
 
-    print(f"Audio for {vid.title} is successfully downloaded w/ rate : {rate}, size : {aud.get_filesize()}")
     return path
 
 
-def get_transcript(url:str, lang:str) -> tuple(list, bool):
+def get_transcript(url:str, lang:str) -> tuple((list, bool)):
     """Get transcript using video id from given url
     Args:
         url (str): Path to youtube link.
         lang (str): Designated language for transcript.
     Returns:
-        tuple(list, bool): Tuple consists of full transcription and handcrafted flag.
+        tuple((list, bool)): Tuple consists of full transcription and handcrafted flag.
     """
-
     if isinstance(url, str):
         id = url.split('/')[-1]
         transcript_list = YouTubeTranscriptApi.list_transcripts(id)
@@ -76,45 +74,37 @@ def get_transcript(url:str, lang:str) -> tuple(list, bool):
     return transcript.fetch(), transcript.is_generated             # is_generated : True if it has been generated automatically or False otherwise (manually created).
 
 
-def make_chunks(a_path:str, t_result:tuple(list, bool), d_path:str):
+def make_chunks(a_path:str, t_result:tuple((list, bool)), d_path:str):
     """Divide raw data into multiple chunks and save them into csv format
     Args:
         a_path (str): Path returned from get_audio() method.
         t_result (tuple): Tuple returned from get_transcript() method.
         d_path (str): Path to destination.
     """
-
     transcript, generated = t_result
-    df = pd.DataFrame()
+    d_list = []
+    for chunk in tqdm(transcript):
+        d_list.append([a_path, chunk['text'], chunk['start'], chunk['duration']])
 
-    for i, chunk in tqdm(enumerate(transcript)):
-        t1 = chunk['start'] * 1000                      # works in milliseconds
-        t2 = t1 + chunk['duration'] * 1000
-        new_aud = AudioSegment.from_wav(a_path)
-        new_aud = new_aud[t1:t2]
-        new_path = f'{a_path}_{i}'
-        new_aud.export(new_path, format="wav")          # exports to a wav file in the current path.
-
-        df['audio'] = new_path
-        df['text'] = chunk['text']
-    
+    df = pd.DataFrame(d_list, columns =['audio', 'text', 'start', 'duration'])
     df['handcrafted'] = not generated
+    df['handcrafted'] = df['handcrafted'].astype(int)
 
-    if os.path.isfile(d_path):               # Updata data file if it exists
-        data = pd.read_csv(d_path)
-        data = pd.concat([data, df])
-        data.to_csv(d_path)
-    else:                                       # Make data file if it doesn't exist
-        os.makedirs(d_path)
+    if os.path.isfile(d_path):          # updata data file if it exists
+        df_0 = pd.read_csv(d_path)
+        df_0 = pd.concat([df_0, df])
+        df_0.to_csv(d_path)
+    else:                               # make data file if it doesn't exist
         df.to_csv(d_path)
-    pass
+
+    return
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--url', type=str, default='sw_test_nonfreeze')    # Project name for wandb
-    parser.add_argument('--language', type=str, default='en')    # Project name for wandb
+    parser.add_argument('--url', type=str, default='sw_test_nonfreeze')
+    parser.add_argument('--language', type=str, default='en')               
     parser.add_argument('--data_path', type=str, default='./Data/data.csv')
     args = parser.parse_args()
     url  = args.url
