008aa3ac86b0b840e1b3ea538ccb9b660b61fdcb
sw-programmer <swgger@kaist.ac.kr>
2023-05-16 07:19:07 +0000

    Add docstring & Implement initial make_chunks() method

--- collect_data.py
+++ collect_data.py
@@ -12,8 +12,15 @@ import yt_dlp as youtube_dl
 from scipy.io.wavfile import read as read_wav
 from youtube_transcript_api import YouTubeTranscriptApi
 
-#### get .wav file from given url ####
-def get_audio(url):
+
+def get_audio(url:str) -> str:
+    """Get .wav file from given url
+    Args:
+        url (str): Path to youtube link.
+    Returns:
+        str: Path to full audio data.
+    """
+
     if isinstance(url, str):
         ydl_opts = {
             'format': 'bestaudio/best',
@@ -37,14 +44,22 @@ def get_audio(url):
         aud_seg.export(path, format='wav')
 
     #TODO: url list 로 주어질 경우 고려 필요
-    else:           
+    else:
         pass
 
     print(f"Audio for {vid.title} is successfully downloaded w/ rate : {rate}, size : {aud.get_filesize()}")
     return path
 
-#### get transcript using video id from given url ####
-def get_transcript(url, lang):
+
+def get_transcript(url:str, lang:str) -> tuple(list, bool):
+    """Get transcript using video id from given url
+    Args:
+        url (str): Path to youtube link.
+        lang (str): Designated language for transcript.
+    Returns:
+        tuple(list, bool): Tuple consists of full transcription and handcrafted flag.
+    """
+
     if isinstance(url, str):
         id = url.split('/')[-1]
         transcript_list = YouTubeTranscriptApi.list_transcripts(id)
@@ -59,12 +74,39 @@ def get_transcript(url, lang):
         pass
 
     return transcript.fetch(), transcript.is_generated             # is_generated : True if it has been generated automatically or False otherwise (manually created).
-    
 
-#### divide raw data into multiple chunks and save them into csv format ####
-def make_chunks(path, transcript):
+
+def make_chunks(a_path:str, t_result:tuple(list, bool), d_path:str):
+    """Divide raw data into multiple chunks and save them into csv format
+    Args:
+        a_path (str): Path returned from get_audio() method.
+        t_result (tuple): Tuple returned from get_transcript() method.
+        d_path (str): Path to destination.
+    """
+
+    transcript, generated = t_result
+    df = pd.DataFrame()
+
+    for i, chunk in tqdm(enumerate(transcript)):
+        t1 = chunk['start'] * 1000                      # works in milliseconds
+        t2 = t1 + chunk['duration'] * 1000
+        new_aud = AudioSegment.from_wav(a_path)
+        new_aud = new_aud[t1:t2]
+        new_path = f'{a_path}_{i}'
+        new_aud.export(new_path, format="wav")          # exports to a wav file in the current path.
+
+        df['audio'] = new_path
+        df['text'] = chunk['text']
     
-    # Save them into csv file (update file it already exists)
+    df['handcrafted'] = not generated
+
+    if os.path.isfile(d_path):               # Updata data file if it exists
+        data = pd.read_csv(d_path)
+        data = pd.concat([data, df])
+        data.to_csv(d_path)
+    else:                                       # Make data file if it doesn't exist
+        os.makedirs(d_path)
+        df.to_csv(d_path)
     pass
 
 
@@ -73,14 +115,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', type=str, default='sw_test_nonfreeze')    # Project name for wandb
     parser.add_argument('--language', type=str, default='en')    # Project name for wandb
+    parser.add_argument('--data_path', type=str, default='./Data/data.csv')
     args = parser.parse_args()
     url  = args.url
     lang = args.language
+    d_path = args.data_path
 
     if not isinstance(url, (str, list)):
         raise TypeError("Wrong url type is given. Try str for single url or list for multiple instead.")
 
-    aud_path = get_audio(url)
-    transcript, generated = get_transcript(url, lang)
-    make_chunks(aud_path, transcript)
-    
+    a_path = get_audio(url)
+    t_result = get_transcript(url, lang)
+    make_chunks(a_path, t_result, d_path)
